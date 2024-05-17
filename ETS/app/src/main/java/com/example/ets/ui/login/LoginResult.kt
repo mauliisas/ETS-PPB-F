0b00199a8f08626a537739260798690b8e5539bc
Sastiara Maulikh <91389347+mauliisas@users.noreply.github.com>
2024-05-17 22:41:24 +0700

    Add files via upload

---
+++ ETS/app/src/main/java/com/example/ets/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.ets.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
